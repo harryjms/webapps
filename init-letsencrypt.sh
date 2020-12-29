a1214addd6b92ffe7220c42048cf18db119b9acd
Harry Jarman <me@harryjms.uk>
2020-12-29 21:47:19 +0000

    HOTFIX cert

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(jbs.harryjms.uk www.harryjms.uk harryjms.uk)
+domains=(*.harryjms.uk)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="me@harryjms.uk" # Adding a valid address is strongly recommended
