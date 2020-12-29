999746448059071c0a0e3524bf2c47d34614b185
Harry Jarman <harry.jarman@bbc.co.uk>
2020-12-29 19:25:16 +0000

    GA Page view

--- apps/jmb/src/pages/_app.tsx
+++ apps/jmb/src/pages/_app.tsx
@@ -1,17 +1,24 @@
 import { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import { useEffect } from "react";
 import Footer from "../components/Footer";
-import { initGA } from "../components/GA";
+import { initGA, logPageView } from "../components/GA";
 import Header from "../components/Header/indx";
 import "../styles/styles.scss";
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
   useEffect(() => {
     if (!window.GA_INITIALIZED && process.env.GOOGLE_ANALYTICS_ID) {
       initGA(process.env.GOOGLE_ANALYTICS_ID);
       window.GA_INITIALIZED = true;
     }
   }, []);
+
+  useEffect(() => {
+    logPageView();
+  }, [router.pathname]);
+
   return (
     <div>
       <Header />
