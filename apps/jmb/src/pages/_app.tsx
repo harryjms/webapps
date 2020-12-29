77d0e4fe143e264cdc66ff8b14f55f6f0c6f1794
Harry Jarman <harry.jarman@bbc.co.uk>
2020-12-29 19:22:03 +0000

    GA

--- apps/jmb/src/pages/_app.tsx
+++ apps/jmb/src/pages/_app.tsx
@@ -1,9 +1,17 @@
 import { AppProps } from "next/app";
+import { useEffect } from "react";
 import Footer from "../components/Footer";
+import { initGA } from "../components/GA";
 import Header from "../components/Header/indx";
 import "../styles/styles.scss";
 
 const App = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    if (!window.GA_INITIALIZED && process.env.GOOGLE_ANALYTICS_ID) {
+      initGA(process.env.GOOGLE_ANALYTICS_ID);
+      window.GA_INITIALIZED = true;
+    }
+  }, []);
   return (
     <div>
       <Header />
